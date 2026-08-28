a6931d3ead9221f5edea37a0cbe2874b1811aa7f
raetsch-hue <raetsch@gmail.com>
2026-08-28 17:40:16 +0200

    Projektstruktur AITM 08/2026 DR

---
+++ coursebook/dateien.js
@@ -0,0 +1,15 @@
+/* ------------------------------------------------------------------
+   Liste der Dokumente, die viewer.html anzeigt.
+
+   Neue Datei?  Hier eine Zeile ergänzen.  Datei gelöscht?  Zeile raus.
+   Das ist die EINZIGE Stelle, die gepflegt werden muss.
+
+   - Pfade sind relativ zu viewer.html
+   - Reihenfolge = Anzeigereihenfolge; der erste Eintrag wird beim
+     Öffnen automatisch geladen
+   - Ein Komma zu viel am Ende ist hier erlaubt (anders als in JSON)
+   ------------------------------------------------------------------ */
+
+var DOKUMENTE = [
+  { datei: "2.1/01_Change-Management-Lewin-ADKAR.md", titel: "Vertiefung: Lewin & ADKAR" },
+];
