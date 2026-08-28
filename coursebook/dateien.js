9e983ecd61959212b3cb3d7e89651d423768bd78
raetsch-hue <raetsch@gmail.com>
2026-08-28 20:11:32 +0200

    Projektstruktur AITM 08/2026 DR

--- coursebook/dateien.js
+++ coursebook/dateien.js
@@ -5,8 +5,10 @@
    Das ist die EINZIGE Stelle, die gepflegt werden muss.
 
    - Pfade sind relativ zu viewer.html
-   - Reihenfolge = Anzeigereihenfolge; der erste Eintrag wird beim
-     Öffnen automatisch geladen
+   - Die Reihenfolge hier ist egal: der Viewer sortiert aufsteigend nach
+     der Nummer im Pfad (1.4 < 1.5 < 1.10 < 2.1)
+   - Beim Öffnen erscheint das zuletzt gelesene Dokument, beim ersten
+     Besuch das erste der sortierten Liste
    - Ein Komma zu viel am Ende ist hier erlaubt (anders als in JSON)
    ------------------------------------------------------------------ */
 
